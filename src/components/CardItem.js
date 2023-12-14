c68fe997ddf82f7821bba4aede00705644107550
yangC <yangyangfrance@hotmail.com>
2023-12-14 16:55:51 -0600

    css file modified

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -2,9 +2,16 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function CardItem(props) {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0, // Scroll to the top of the window
+      left: 0,
+      behavior: "smooth", // Optional: Defines smooth scrolling
+    });
+  };
   return (
     <>
-      <li className="cards__item">
+      <li className="cards__item" onClick={scrollToTop}>
         <Link className="cards__item__link" to={props.path}>
           <figure className="cards__item__pic-wrap" data-category={props.label}>
             <img className="cards__item__img" alt="Project" src={props.src} />
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./contact.css";
 
 const ContactMe = () => {
   return (
     <div className="contact-me-container">
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSf9DzgCvgsIrSvSB8_koDwB5YO_FA7ULf6xsTJRLEDoMuDalw/viewform?embedded=true"
-        width="1500"
-        height="993"
+        width="100%"
+        height="100%"
         title="Contact Me Form" // Adding a title for accessibility reasons
       >
         Loading…
